b5b6b7298bc5296340a5cbabffd460cca5a43e51
Ganesh sai <ganeshsqai1346@gmail.com>
2022-05-06 14:55:10 +0200

    solving basics of JS

---
+++ soloproject.js
@@ -0,0 +1,38 @@
+/* EXERCISE A
+Create a variable called test and assign a string value to it.
+*/
+
+let test = "COVID RT-PCR"
+
+/* EXERCISE B
+Create a variable called sum and assign to it the result of the sum between the numbers 10 and 20.
+*/
+
+let number = 20
+let sum = 0
+for  (let i = 10; i <= 20; i++) {
+    sum += i
+}
+console.log(sum)
+
+/* EXERCISE C
+Create a variable called random and assign to it a random number between 0 and 20 (it should be randomly created at each execution).
+*/
+
+let random 
+random = Math.floor(Math.random() * 20)
+console.log(random)
+
+/* EXERCISE D
+Create a variable called me and assign to it an object containing the following information: name = your name, surname = your surname, age = your age.
+*/
+
+let me = {
+    name : "Ganesh Sai",
+    surname : "Devarakonda",
+    age : 23
+}
+console.log(me.surname)
+
+
+ 
